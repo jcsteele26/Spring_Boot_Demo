0695e9cd06edc20f03efa39cea84d60c29d9fb1e
Andre <jcsteele26@gmail.com>
2020-06-21 15:25:02 -0400

    Spring Boot Demo files

---
+++ springdemo/src/test/java/com/steele/springdemo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steele.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
